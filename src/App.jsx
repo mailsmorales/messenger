41216d1c39b9a1d315ec4cd05d4dc79dde90f626
mukhamed-Ali <mailsmorales21@mail.ru>
2022-07-27 17:58:33 +0600

    new-file-header-nav-index.css add style base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Header } from './components/Header/Header'
 
 function App() {
   return (
-   <h1>Hello World</h1>
+  <div className="App">
+    <Header />
+  </div>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "./Nav/Nav";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="header">
+        <Nav />
+      </div>
+    </header>
+  );
+};
